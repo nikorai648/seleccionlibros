bc41bbad47e0c39abd7d6029004426f5af09c651
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-20 23:28:29 -0300

    viernes

---
+++ src/Firebase/credenciales.tsx
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD5Zps_Bylb4cDfeho-dMwv5yZ4LK4jd5o",
+    authDomain: "seleccionlibros-d0f52.firebaseapp.com",
+    projectId: "seleccionlibros-d0f52",
+    storageBucket: "seleccionlibros-d0f52.firebasestorage.app",
+    messagingSenderId: "936470754010",
+    appId: "1:936470754010:web:13f6a1816cf7adbed56be3"
+  };
