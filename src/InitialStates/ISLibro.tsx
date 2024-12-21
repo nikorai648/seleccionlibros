71101bd65609978be89eb0d18fd876e41f41ca3d
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-21 17:09:02 -0300

    sabado

---
+++ src/InitialStates/ISLibro.tsx
@@ -0,0 +1,13 @@
+import { ILibro } from "@/Interfaces/ILibro";
+
+export const iSLibro:ILibro = {
+    nombre:'',
+    autor:'',
+    editorial:'',
+    anio:0,
+    valoracion:0,
+    pais:'',
+    sexomasculino:'',
+    sexofemenino:'',
+    genero:''
+}
---
+++ src/Interfaces/ILibro.tsx
@@ -0,0 +1,12 @@
+export interface ILibro{
+   nombre:string
+   autor: string
+   editorial: string
+   anio: number
+   valoracion: number
+   pais: string
+   sexomasculino: string
+   sexofemenino: string
+   genero: string
+
+}
