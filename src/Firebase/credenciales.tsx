01dbb121ee9283f9a3ae17a6bdc2013244cc635b
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-20 23:40:43 -0300

    Update credenciales.tsx

--- src/Firebase/credenciales.tsx
+++ src/Firebase/credenciales.tsx
@@ -1,6 +1,5 @@
-// credenciales.tsx
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyD5Zps_Bylb4cDfeho-dMwv5yZ4LK4jd5o",
     authDomain: "seleccionlibros-d0f52.firebaseapp.com",
     projectId: "seleccionlibros-d0f52",
