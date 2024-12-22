a68ee1b405ed6647ed40b81e9700a4b490853cce
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2024-12-22 18:36:56 -0300

    domingo

--- src/app/FormularioLibroFinal.tsx
+++ src/app/FormularioLibroFinal.tsx
@@ -36,12 +36,111 @@ export const FormularioLibroFinal=() => {
             <p>Sexo: {libro.sexomasculino}</p>
             <p>Sexo: {libro.sexofemenino}</p>
             <p>Género: {libro.genero}</p>
-        
-        
-        
-        
-        
-        
+
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Nombre</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese un nombre"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Autor</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese un nombre de autor"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Editorial</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese un editorial"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Año</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese un año"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>Valoración</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese una valoracion"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+             <Form.Label>País</Form.Label>
+             <Form.Control type="email" placeholder="Ingrese un pais"
+              name='nombre'
+        onChange={((e)=>{handleLibro(e.currentTarget.name,e.currentTarget.value)})}
+        />
+      </Form.Group>
+
+      {/* Sexos */}
+      <Form.Group className="mb-3">
+                    <Form.Label>Sexo Masculino</Form.Label>
+                    <Form.Check
+                        type="radio"
+                        label="Sí"
+                        name="sexomasculino"
+                        value="Sí"
+                        checked={libro.sexomasculino === "Sí"}
+                        onChange={(e) => handleLibro(e.currentTarget.name, e.currentTarget.value)}
+                    />
+                    <Form.Check
+                        type="radio"
+                        label="No"
+                        name="sexomasculino"
+                        value="No"
+                        checked={libro.sexomasculino === "No"}
+                        onChange={(e) => handleLibro(e.currentTarget.name, e.currentTarget.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Sexo Femenino</Form.Label>
+                    <Form.Check
+                        type="radio"
+                        label="Sí"
+                        name="sexofemenino"
+                        value="Sí"
+                        checked={libro.sexofemenino === "Sí"}
+                        onChange={(e) => handleLibro(e.currentTarget.name, e.currentTarget.value)}
+                    />
+                    <Form.Check
+                        type="radio"
+                        label="No"
+                        name="sexofemenino"
+                        value="No"
+                        checked={libro.sexofemenino === "No"}
+                        onChange={(e) => handleLibro(e.currentTarget.name, e.currentTarget.value)}
+                    />
+                </Form.Group>
+
+                {/* Género */}
+                <Form.Group className="mb-3">
+                    <Form.Label>Género</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ingrese el género"
+                        name="genero"
+                        value={libro.genero}
+                        onChange={(e) => handleLibro(e.currentTarget.name, e.currentTarget.value)}
+                    />
+                </Form.Group>
+                <Button variant="primary" onClick={handleRegistrar}> Registrar Libro</Button>
+            </Form>
         </>
-       
+    );
 }
+export default FormularioLibroFinal
